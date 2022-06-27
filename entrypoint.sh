9c79e182a95e24817ed5ae3798b3121744a5120e
Arhur-TRT <atrouill@student.42.fr>
2022-06-27 14:36:02 -0700

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 
-#cp -R -u /cache/node_modules /api/node_modules
-ln -s /cache/node_modules /api/node_modules
+cp -r /cache/node_modules/. /api/node_modules/
+#ln -s /cache/node_modules /api/node_modules
 exec npm run start:dev
